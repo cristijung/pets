f1cd8e7619c464e8fb2a874e727295446b3539b4
CristinaJung <cristijung@gmail.com>
2026-03-24 15:55:31 -0300

    imagem

--- components/uiCommon/header/Header.tsx
+++ components/uiCommon/header/Header.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
 import NavBar from "../navBar/NavBar";
 import styles from "./Header.module.css";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
-        {/* colocar 1 logo aqui */}
+        <Link href='/' className={styles.logoArea}>
+        <Image src='/logo_sw.png' alt='Logo' width={180} height={50} priority quality={75}/>
+        <img src='/logo_sw.png' alt='abc' width={180} height={50} />   
+        </Link>
         <div className={styles.logoArea}>Ponto dos Pets</div>
         <NavBar />
       </div>
